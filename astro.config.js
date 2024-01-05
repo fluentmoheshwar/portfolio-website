a96ec02564a6a3c2674c8d01c9c3fbba7e2743c9
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2024-01-05 14:40:24 +0600

    icon: use react icons

--- astro.config.js
+++ astro.config.js
@@ -1,32 +1,29 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
-
 import browserslist from "browserslist";
 import { browserslistToTargets } from "lightningcss";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fluentmoheshwar.pages.dev",
   compressHTML: true,
   build: {
-    inlineStylesheets: "auto",
+    inlineStylesheets: "auto"
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), react()],
   vite: {
     css: {
       lightningcss: {
-        targets: browserslistToTargets(browserslist("defaults")),
-      },
+        targets: browserslistToTargets(browserslist("defaults"))
+      }
     },
     build: {
-      cssMinify: "lightningcss",
-    },
-  },
+      cssMinify: "lightningcss"
+    }
+  }
 });
