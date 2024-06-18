520a1216cbbe5b453c7b4929385699089860b67d
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2024-06-18 20:47:28 +0600

    added idx config file

---
+++ .idx/dev.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_22
+    pkgs.bun
+  ];
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "astro-build.astro-vscode"
+    "esbenp.prettier-vscode"
+    "bradlc.vscode-tailwindcss"
+    "streetsidesoftware.code-spell-checker"
+    "davidanson.vscode-markdownlint"
+  ]
+
+  # Enable previews and customize configuration
+  idx.previews = {
+    enable = true;
+    previews = {
+      web = {
+        command = [
+          "bun"
+          "dev"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
