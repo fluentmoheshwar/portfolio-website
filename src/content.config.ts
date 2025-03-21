5177c81e80fb1f6849439c8f6c2de5f326badf6f
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2025-03-22 01:09:30 +0600

    feat: allow multiple languages

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,6 +4,7 @@ import { glob } from "astro/loaders";
 const postsCollection = defineCollection({
   loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/data/posts" }),
   schema: z.object({
+    lang: z.string(),
     title: z.string(),
     description: z.string(),
     socialImage: z.string(),
